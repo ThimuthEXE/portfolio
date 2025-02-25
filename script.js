9ef546b8f50c43114462721a2a36b852d86984d6
ThimuthEXE <thimuthexe@gmail.com>
2025-02-25 22:53:19 +0530

    Basic page design

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded Successfully");
+});
+document.querySelector(".custom-button").addEventListener("click", () => {
+    alert("Button Clicked!");
+});
