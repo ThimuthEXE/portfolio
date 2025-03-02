af05e5fa247260c374d2c8d41bfa242363d5a691
ThimuthEXE <thimuthexe@gmail.com>
2025-03-02 22:09:10 +0530

    re organizing

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Website Loaded Successfully");
 
-    // Scroll Event for Background Change
-    window.addEventListener("scroll", () => {
-        const scrollPosition = window.scrollY;
-        const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
-        const scrollPercentage = (scrollPosition / documentHeight) * 100;
-
-        // Adjust the background position based on scroll percentage
-        document.body.style.backgroundPosition = `0% ${scrollPercentage}%`;
-    });
-
     const menuIcon = document.querySelector(".menu-icon");
     const dropdownMenu = document.querySelector(".dropdown-menu");
 
